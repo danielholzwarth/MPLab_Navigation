947c26c59e1a13034f2b0212739859e4f57a99ca
danielholzwarth <danielholzwarth@gmx.net>
2023-12-16 14:52:49 +0100

    feat: implement frame

--- app/src/main/java/com/example/mplab_navigation/MainActivity.kt
+++ app/src/main/java/com/example/mplab_navigation/MainActivity.kt
@@ -1,46 +1,54 @@
 package com.example.mplab_navigation
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mplab_navigation.ui.theme.MPLab_NavigationTheme
+import android.preference.PreferenceManager
+import android.widget.Button
+import android.widget.Toast
+import org.osmdroid.config.Configuration.getInstance
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : Activity() {
+    private lateinit var map : MapView
+    private lateinit var mLocationOverlay: MyLocationNewOverlay
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MPLab_NavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+        setContentView(R.layout.activity_main)
+
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+
+        val mapController = map.controller
+        mapController.setZoom(18)
+
+        this.mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this.applicationContext), map)
+        this.mLocationOverlay.enableMyLocation()
+        map.overlays.add(this.mLocationOverlay)
+
+        val startPoint = GeoPoint(49.12, 9.21)
+        map.controller.setCenter(startPoint)
+
+        val searchBtn = findViewById<Button>(R.id.searchButton)
+        searchBtn.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Not implemented yet.", Toast.LENGTH_LONG).show()
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        val centerBtn = findViewById<Button>(R.id.centerButton)
+        centerBtn.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Not implemented yet.", Toast.LENGTH_LONG).show()
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MPLab_NavigationTheme {
-        Greeting("Android")
+        val startRouteBtn = findViewById<Button>(R.id.startRouteButton)
+        startRouteBtn.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Not implemented yet.", Toast.LENGTH_LONG).show()
+        }
     }
 }
