e7590f0cff0f373ebae24ad4ecfaea424cc5a66b
danielholzwarth <danielholzwarth@gmx.net>
2023-12-16 15:45:29 +0100

    feat: implement navigation

--- app/src/main/java/com/example/mplab_navigation/MainActivity.kt
+++ app/src/main/java/com/example/mplab_navigation/MainActivity.kt
@@ -4,14 +4,18 @@ import android.app.Activity
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
+import android.os.StrictMode
 import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import org.osmdroid.bonuspack.routing.OSRMRoadManager
+import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.config.Configuration.getInstance
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Polyline
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import java.util.Locale
@@ -20,10 +24,15 @@ import java.util.Locale
 class MainActivity : Activity() {
     private lateinit var map : MapView
     private lateinit var mLocationOverlay: MyLocationNewOverlay
+    private lateinit var targetLocation: GeoPoint
+    private lateinit var routeOverlay: Polyline
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
         setContentView(R.layout.activity_main)
 
@@ -43,8 +52,11 @@ class MainActivity : Activity() {
         val searchBtn = findViewById<Button>(R.id.searchButton)
         searchBtn.setOnClickListener {
             val input = findViewById<EditText>(R.id.searchEditText).text.toString()
-            if(input != ""){
-                moveCameraToLocation(input)
+            if(input.isNotEmpty()){
+                getGeoPointFromString(input)?.let { target ->
+                    targetLocation = target
+                    moveCameraToLocation()
+                }
             } else{
                 Toast.makeText(this.applicationContext,"Input can not be empty!",Toast.LENGTH_LONG).show()
             }
@@ -57,26 +69,68 @@ class MainActivity : Activity() {
 
         val startRouteBtn = findViewById<Button>(R.id.startRouteButton)
         startRouteBtn.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Not implemented yet.", Toast.LENGTH_LONG).show()
+            val input = findViewById<EditText>(R.id.searchEditText).text.toString()
+            if(input.isNotEmpty()){
+                getGeoPointFromString(input)?.let { target ->
+                    targetLocation = target
+                    moveCameraToLocation()
+
+                    //drawLine()
+                    drawRoute()
+                }
+            } else {
+                Toast.makeText(this.applicationContext,"Input can not be empty!",Toast.LENGTH_LONG).show()
+            }
         }
     }
 
-    private fun moveCameraToLocation(locationName: String){
+    private fun getGeoPointFromString(locationName: String): GeoPoint? {
         try {
             val geocoder = Geocoder(this.applicationContext, Locale.getDefault())
             val geoResults: MutableList<Address>? = geocoder.getFromLocationName(locationName, 1)
             if (geoResults != null) {
                 if (geoResults.isNotEmpty()) {
                     val address = geoResults[0]
-                    val location = GeoPoint(address.latitude, address.longitude)
-                    this.mLocationOverlay.disableFollowLocation()
-                    map.controller.animateTo(location)
-                }else{
+                    return GeoPoint(address.latitude, address.longitude)
+                } else {
                     Toast.makeText(this.applicationContext,"Location Not Found",Toast.LENGTH_LONG).show()
                 }
             }
         } catch (e: java.lang.Exception) {
             print(e.message)
         }
+        return null
+    }
+
+    private fun moveCameraToLocation(){
+        this.mLocationOverlay.disableFollowLocation()
+        map.controller.animateTo(targetLocation)
+    }
+
+    private fun drawLine() {
+        val path = Polyline()
+        path.setColor(0xFF0000FF.toInt())
+        path.setWidth(5.0f)
+        path.setPoints(listOf(mLocationOverlay.myLocation, targetLocation))
+        map.overlays.add(path)
+        routeOverlay = path
+
+        map.invalidate()
+    }
+
+    private fun drawRoute() {
+        val roadManager: RoadManager = OSRMRoadManager(this, "Manager")
+        val waypoints = ArrayList<GeoPoint>()
+        waypoints.add(mLocationOverlay.myLocation)
+        waypoints.add(targetLocation)
+
+        val road = roadManager.getRoad(waypoints)
+        routeOverlay = RoadManager.buildRoadOverlay(road)
+
+        routeOverlay.width = 15.0f
+
+        map.overlays.add(routeOverlay)
+
+        map.invalidate()
     }
 }
