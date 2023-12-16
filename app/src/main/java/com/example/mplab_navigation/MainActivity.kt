b8ad0edaf06116822f82eefada824491d1aca83e
danielholzwarth <danielholzwarth@gmx.net>
2023-12-16 15:28:51 +0100

    feat: implement search

--- app/src/main/java/com/example/mplab_navigation/MainActivity.kt
+++ app/src/main/java/com/example/mplab_navigation/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.mplab_navigation
 
 import android.app.Activity
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import org.osmdroid.config.Configuration.getInstance
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -11,6 +14,7 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import java.util.Locale
 
 
 class MainActivity : Activity() {
@@ -38,17 +42,41 @@ class MainActivity : Activity() {
 
         val searchBtn = findViewById<Button>(R.id.searchButton)
         searchBtn.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Not implemented yet.", Toast.LENGTH_LONG).show()
+            val input = findViewById<EditText>(R.id.searchEditText).text.toString()
+            if(input != ""){
+                moveCameraToLocation(input)
+            } else{
+                Toast.makeText(this.applicationContext,"Input can not be empty!",Toast.LENGTH_LONG).show()
+            }
         }
 
         val centerBtn = findViewById<Button>(R.id.centerButton)
         centerBtn.setOnClickListener {
-            Toast.makeText(this.applicationContext,"Not implemented yet.", Toast.LENGTH_LONG).show()
+            this.mLocationOverlay.enableFollowLocation()
         }
 
         val startRouteBtn = findViewById<Button>(R.id.startRouteButton)
         startRouteBtn.setOnClickListener {
             Toast.makeText(this.applicationContext,"Not implemented yet.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun moveCameraToLocation(locationName: String){
+        try {
+            val geocoder = Geocoder(this.applicationContext, Locale.getDefault())
+            val geoResults: MutableList<Address>? = geocoder.getFromLocationName(locationName, 1)
+            if (geoResults != null) {
+                if (geoResults.isNotEmpty()) {
+                    val address = geoResults[0]
+                    val location = GeoPoint(address.latitude, address.longitude)
+                    this.mLocationOverlay.disableFollowLocation()
+                    map.controller.animateTo(location)
+                }else{
+                    Toast.makeText(this.applicationContext,"Location Not Found",Toast.LENGTH_LONG).show()
+                }
+            }
+        } catch (e: java.lang.Exception) {
+            print(e.message)
         }
     }
 }
